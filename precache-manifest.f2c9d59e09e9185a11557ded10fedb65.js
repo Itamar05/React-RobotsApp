2106083271ae921c4c4094e85d27df3670107372
Itamar05 <itamar05@icloud.com>
2020-02-25 11:10:05 +0100

    Updates

---
+++ precache-manifest.f2c9d59e09e9185a11557ded10fedb65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928786a6a72f0a72fbe88a6ca7853b34",
+    "url": "/React-RobotsApp/index.html"
+  },
+  {
+    "revision": "91e7bc4165c309dafe4a",
+    "url": "/React-RobotsApp/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "46f144ccedbc180d7005",
+    "url": "/React-RobotsApp/static/css/main.da8965c0.chunk.css"
+  },
+  {
+    "revision": "91e7bc4165c309dafe4a",
+    "url": "/React-RobotsApp/static/js/2.bad5ebae.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-RobotsApp/static/js/2.bad5ebae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f144ccedbc180d7005",
+    "url": "/React-RobotsApp/static/js/main.3ab57a38.chunk.js"
+  },
+  {
+    "revision": "39083217c5073e9f58a6",
+    "url": "/React-RobotsApp/static/js/runtime-main.3bf20731.js"
+  }
+]);
